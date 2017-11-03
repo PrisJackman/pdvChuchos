eaf8f7db1269c1fd8b3517806202dedd34cfc444
Priscilla Benito Anselmo <pba_deagh@hotmail.com>
2017-11-03 13:06:24 -0700

    rutas

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,11 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
+/*Mas opciones url*/
 Route::get('/registrar_productos', 'productosController@vista_registro');
+Route::get('/registrar_categorias', 'categoriasController@vista_registroc');
+Route::get('/registrar_matprima', 'matprimaController@vista_registrom');
+Route::get('/consulta_productos', 'productosController@vista_productos');
+
+/*Caja*/
+Route::get('/caja', 'cajaController@vista_caja');
