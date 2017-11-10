bb2c89eb7a08926650d82a3d4730773898b4b6b8
Jasiel1995 <jasieldavid@hotmail.com>
2017-11-10 09:44:27 -0700

    Vista registrar producto terminada-consultar con el equipo algunos detalles

--- resources/views/vista_regprod.blade.php
+++ resources/views/vista_regprod.blade.php
@@ -4,11 +4,17 @@
 <div class="jumbotron" align="center">
 	<form>
   <fieldset>
-    <legend>Legend</legend>
+    <legend>Registrar Producto</legend>
     <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" type="email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      <label for="exampleInputEmail1">Descripcion del producto</label>
+      <input class="form-control" id="" aria-describedby="emailHelp" placeholder="Escribir Descripcion" type="text">
+      <label for="exampleInputEmail1">ID de la Categoria</label>
+      <input class="form-control" id="" aria-describedby="emailHelp" placeholder="Escribir el ID" type="text">
+      <label for="exampleInputEmail1">Precio Unitario</label>
+      <input class="form-control" id="" aria-describedby="emailHelp" placeholder="Escribir Precio" type="text">
+      <br>
+      <button class="btn btn-default" id="btnCategoria">Guardar</button> 
+      
     </div>
 	</form>
   </fieldset>
